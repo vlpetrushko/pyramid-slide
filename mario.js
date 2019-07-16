16f512c8b57ca2d2d78f0fdb4146ea9556a044cf
Uladzimir Piatruška <uladzimir@mail.mail>
2019-07-17 00:01:11 +0300

    рука бога 2
    1. обозначили const неизменяемые значения
    2. разделили создание данных и их вывод

--- mario.js
+++ mario.js
@@ -1,10 +1,10 @@
 function printPyramid(height, filler) {
-    var pyramid = document.getElementById('pyramid');
+    const pyramid = document.getElementById('pyramid');
     pyramid.innerHTML = '';
     for (let i = 0; i < height; i++) {
-        let line = document.createElement('p');
-        line.innerHTML = ''.padEnd(height - 1 - i, "\u00A0").padEnd(height + 1, filler);
+        const row = ''.padEnd(height - 1 - i, "\u00A0").padEnd(height + 1, filler);
+        const line = document.createElement('p');
+        line.innerHTML = row;
         pyramid.appendChild(line);
     }
-
 }
