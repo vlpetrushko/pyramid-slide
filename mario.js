a6fb8a1baf6229a83e9cc3363816eee8fc68165c
Uladzimir Piatruška <uladzimir@mail.mail>
2019-07-16 23:53:49 +0300

    рука бога циклы заменены на padEnd

--- mario.js
+++ mario.js
@@ -1,16 +1,10 @@
 function printPyramid(height, filler) {
-    let str = '';
-    for (let i = 0; i < height - 1; i++) {
-        str = str.concat("\u00A0");
-    }
-    str = str.concat(filler + filler);
     var pyramid = document.getElementById('pyramid');
     pyramid.innerHTML = '';
     for (let i = 0; i < height; i++) {
         let line = document.createElement('p');
-        line.innerHTML += str;
+        line.innerHTML = ''.padEnd(height - 1 - i, "\u00A0").padEnd(height + 1, filler);
         pyramid.appendChild(line);
-        str = str.substring(0, height - 2 - i) + filler + str.substring(height - i - 1);
     }
 
 }
