ffdabaaf4451d43f5f74a9d8ffce735cbeb361f9
Uladzimir Piatruška <uladzimir@mail.mail>
2019-07-17 14:02:03 +0300

    added destination parameter to printPyramid

--- mario.js
+++ mario.js
@@ -1,10 +1,9 @@
-function printPyramid(height, filler) {
-    const pyramid = document.getElementById('pyramid');
-    pyramid.innerHTML = '';
+function printPyramid(height, filler, destination) {
+    destination.innerHTML = '';
     for (let i = 0; i < height; i++) {
         const row = ''.padEnd(height - 1 - i, "\u00A0").padEnd(height + 1, filler);
         const line = document.createElement('p');
         line.innerHTML = row;
-        pyramid.appendChild(line);
+        destination.appendChild(line);
     }
 }
